10fcd1101979e3e0c5be266c7dca1cb3809ea4d4
Steinar Vittersoe Kaldager <steinarvk@gmail.com>
2011-04-01 23:59:33 +0200

    changes cleanup

--- binning/bins_test.c
+++ binning/bins_test.c
@@ -3,18 +3,54 @@
 #include <assert.h>
 #include <string.h>
 
+#include "freegetopt-0.11/getopt.h"
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
-    const int B = 3;
-    const int K = 1000;
-    const char *FN = "bintable_test.out";
-    const long long TW = ((1+K)*K)/2;
+    int B = 3;
+    int K = 1000;
+    char *FN = "bintable_test.out";
+    int verbose = 0;
+
+    while(1) {
+        int c = getopt( argc, argv, "vk:o:b:" );
+        if( c < 0 ) break;
+        switch( c ) {
+            case 'k':
+                K = atoi( optarg );
+                break;
+            case 'b':
+                B = atoi( optarg );
+                break;
+            case 'o':
+                FN = optarg;
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            default:
+                fprintf(stderr, "unknown option: '%c'\n", c );
+                exit(1);
+        }
+    }
+
+    long long TW = ((1+K)*K)/2;
+    int argi = optind;
     char buf[1024];
     struct bin_cons *bc = make_bincons( TW, B );
     int i;
 
+    if( verbose ) {
+        fprintf( stderr, "Parameters:\n" );
+        fprintf( stderr, "\tb: %d bins\n", B );
+        fprintf( stderr, "\tk: counting 1 .. %d\n", K );
+        fprintf( stderr, "\t   (total weight %lld)\n", TW );
+        fprintf( stderr, "\to: output to \"%s\"\n", FN );
+    }
+
     assert( bc );
 
-    for(i=1;i<=1000;i++) {
+    for(i=1;i<=K;i++) {
         memset( buf, 0, sizeof buf );
         snprintf( buf, sizeof buf, "%d", i );
         int err = update_bincons( bc, buf, i );
---
+++ binning/btprint.c
@@ -0,0 +1,55 @@
+#include "bins.h"
+
+#include "freegetopt-0.11/getopt.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int verbose = 0;
+    while(1) {
+        int c = getopt( argc, argv, "v" );
+        if( c < 0 ) break;
+        switch( c ) {
+            case 'v':
+                verbose = 1;
+                break;
+            default:
+                fprintf(stderr, "unknown option: '%c'\n", c );
+                exit(1);
+        }
+    }
+    int argi = optind;
+
+    while( argi < argc ) {
+        const char *fn = argv[argi];
+        FILE *f = fopen( fn, "rb" );
+        if( !f ) {
+            fprintf( stderr, "warning: failed to open \"%s\", skipping.\n", fn );
+            continue;
+        }
+        struct bin_table *bt = bintable_read( f );
+        if( !bt ) {
+            fclose( f );
+            fprintf( stderr, "warning: failed to read table in \"%s\", skipping.\n", fn );
+            continue;
+        }
+
+        if( verbose ) {
+            printf( "=== %s ===\n", fn );
+        }
+
+        printf( "%d bins, %lld total weight.\n", bt->no_bins, bt->total_count );
+        for(int i=0;i<bt->no_bins;i++) {
+            printf( "\t%d:\t% 12lld\t\"%s\"\n", i, bt->counts[i], bt->entries[i] );
+        }
+
+        free_bintable( bt );
+
+        fclose( f );
+
+        argi++;
+    }
+
+    return 0;
+}
