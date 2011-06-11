a365f30afc0ff73cc193807cde267b1bcfcb3f77
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-06-11 22:53:05 +0200

    master plan, multiple passes

--- binning/hashbins-to-sfbt.c
+++ binning/hashbins-to-sfbt.c
@@ -14,6 +14,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define DESCRIPTORS_PER_BATCH 1000
+// limit tends to be 1024
+
 long long ipow(long long a, long long b) {
     if( !b ) return 1;
     if( b == 1 ) return a;
@@ -117,94 +120,114 @@ int main(int argc, char* argv[]) {
             fprintf( stderr, "\toutput to:\n" );
         }
     }
+    
+    int passno = 1;
+    long long fdno = 0;
 
-    for(long long j=0;j<maxindex;j++) {
-        long long index = j;
-        for(int k=0;k<N;k++) {
-            int x = index % no_bins;
-            index /= no_bins;
-            xs[ N-1-k ] = x;
+    do {
+        long long dmax = MIN( fdno + DESCRIPTORS_PER_BATCH, maxindex );
+        fprintf( stderr, "Beginning pass %d, covering files %ld to %ld.\n", passno, fdno, dmax-1 );
+        for(long long j=0;j<maxindex;j++) {
+            histogram[j] = 0;
         }
-        assert( N >= 1 );
-        char filename[4096];
-        strcpy( filename, ODN );
-        if( prefix_digits > 0 ) {
-            char buf[1024];
-            strcpy( buf, "" );
-            for(int k=0;k<prefix_digits;k++) {
+        for(long long j=fdno;j<dmax;j++) {
+            long long index = j;
+            for(int k=0;k<N;k++) {
+                int x = index % no_bins;
+                index /= no_bins;
+                xs[ N-1-k ] = x;
+            }
+            assert( N >= 1 );
+            char filename[4096];
+            strcpy( filename, ODN );
+            if( prefix_digits > 0 ) {
+                char buf[1024];
+                strcpy( buf, "" );
+                for(int k=0;k<prefix_digits;k++) {
+                    char buf2[8];
+                    sprintf( buf2, "%c", xs[k] + 'a' );
+                    strcat( buf, buf2 );
+                }
+                strcat( filename, "/" );
+                strcat( filename, buf );
+            }
+            mkdir( filename, 0744 );
+            strcat( filename, "/" );
+            for(int k=prefix_digits;k<N;k++) {
                 char buf2[8];
                 sprintf( buf2, "%c", xs[k] + 'a' );
-                strcat( buf, buf2 );
+                strcat( filename, buf2 );
             }
-            strcat( filename, "/" );
-            strcat( filename, buf );
+            strcat( filename, suffix );
+            if( verbose ) {
+                fprintf( stderr, "\t\t\"%s\"\n", filename );
+            }
+            histogram[j] = sfbt_new_wctx( filename );
+            if( !histogram[j] ) {
+                fprintf( stderr, "fatal error: failed to construct #%d wctx (%s)\n", j, filename );
+                return 1;
+            }
         }
-        mkdir( filename, 0744 );
-        strcat( filename, "/" );
-        for(int k=prefix_digits;k<N;k++) {
-            char buf2[8];
-            sprintf( buf2, "%c", xs[k] + 'a' );
-            strcat( filename, buf2 );
+
+        long long processed = 0;
+
+        for(int j=argi;j<argc;j++) {
+            const char *IFN = argv[j];
+
+            fprintf( stderr, "Reading %d-grams from \"%s\".\n", N, IFN );
+            struct ngr_file *ngrf = ngr_open( IFN );
+            while( ngr_next(ngrf) ) {
+                if( ngr_columns(ngrf) != (N+1) ) {
+                    fprintf( stderr, "fatal error: expected %d columns, got %d\n", N + 1, ngr_columns(ngrf) );
+                    exit(1);
+                }
+                char buffer[ MAX_KEY_SIZE ];
+                long long index = 0;
+                strcpy( buffer, ngr_s_col( ngrf, 0 ) );
+                for(int k=0;k<N;k++) {
+                    const char *s = ngr_s_col( ngrf, k );
+                    index *= no_bins;
+                    index += classify_uint32( no_bins, murmur_hash( s, strlen(s) ) );
+
+                    if( k > 0 ) {
+                        strcat( buffer, " " );
+                        strcat( buffer, s );
+                    }
+                }
+                if( histogram[ index ] ) {
+                    sfbt_add_entry( histogram[ index ], buffer, ngr_ll_col( ngrf, N ) );
+                }
+                ++processed;
+            }
+                fprintf( stderr, "done processing\n" );
+            ngr_free(ngrf);
+                fprintf( stderr, "done freeing\n" );
         }
-        strcat( filename, suffix );
+
         if( verbose ) {
-            fprintf( stderr, "\t\t\"%s\"\n", filename );
+            fprintf( stderr, "%lld %d-grams processed.\n", processed, N );
         }
-        histogram[j] = sfbt_new_wctx( filename );
-        if( !histogram[j] ) {
-            fprintf( stderr, "fatal error: failed to construct #%d wctx (%s)\n", j, filename );
-            return 1;
-        }
-    }
-
-    long long processed = 0;
-
-    for(int j=argi;j<argc;j++) {
-        const char *IFN = argv[j];
-
-        fprintf( stderr, "Reading %d-grams from \"%s\".\n", N, IFN );
-        struct ngr_file *ngrf = ngr_open( IFN );
-        while( ngr_next(ngrf) ) {
-            if( ngr_columns(ngrf) != (N+1) ) {
-                fprintf( stderr, "fatal error: expected %d columns, got %d\n", N + 1, ngr_columns(ngrf) );
-                exit(1);
+        
+                fprintf( stderr, "done printing message\n" );
+        for(long long j=fdno;j<dmax;j++) {
+            if( verbose ) {
+                fprintf( stderr, "Finalizing SFBT %d of %d.\n", j + 1, dmax );
             }
-            char buffer[ MAX_KEY_SIZE ];
-            long long index = 0;
-            strcpy( buffer, ngr_s_col( ngrf, 0 ) );
-            for(int k=0;k<N;k++) {
-                const char *s = ngr_s_col( ngrf, k );
-                index *= no_bins;
-                index += classify_uint32( no_bins, murmur_hash( s, strlen(s) ) );
-
-                if( k > 0 ) {
-                    strcat( buffer, " " );
-                    strcat( buffer, s );
+            if( histogram[j] ) {
+                int rv = sfbt_close_wctx( histogram[j] );
+                if( rv ) {
+                    fprintf( stderr, "fatal error: failure.\n" );
+                    return 1;
                 }
             }
-            sfbt_add_entry( histogram[ index ], buffer, ngr_ll_col( ngrf, N ) );
-            ++processed;
         }
-            fprintf( stderr, "done processing\n" );
-        ngr_free(ngrf);
-            fprintf( stderr, "done freeing\n" );
-    }
 
-    if( verbose ) {
-        fprintf( stderr, "%lld %d-grams processed.\n", processed, N );
-    }
-    
-            fprintf( stderr, "done printing message\n" );
-    for(long long j=0;j<maxindex;j++) {
-        if( verbose ) {
-            fprintf( stderr, "Finalizing SFBT %d of %d.\n", j + 1, maxindex );
-        }
-        int rv = sfbt_close_wctx( histogram[j] );
-        if( rv ) {
-            fprintf( stderr, "fatal error: failure.\n" );
-            return 1;
-        }
-    }
+        fprintf( stderr, "Finished pass %d, covering files %ld to %ld.\n", passno, fdno, dmax-1 );
+        passno++;
+
+        fdno = dmax;
+    } while( fdno < maxindex );
+
 
     free(xs);
     free(histogram);
