6d4c2141811c8db3ed9111e9838ec6382e82dea7
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-06-13 18:57:42 +0200

    fix

--- merging/joinsort.py
+++ merging/joinsort.py
@@ -1,24 +1,22 @@
 from sys import stdin, stderr, argv
+from heapq import heappush, heappop
 
-files = list( map( lambda fn : open(fn, "r"), argv[1:] ) )
-bufs = [ 0 for i in range(len(files)) ]
+files = list( map( lambda fn : [open(fn, "r"), 0], argv[1:] ) )
 q = []
 
 while files:
-    for i in [ i for i in range(len(files)) if bufs[i] <= 0 ]:
-        lines = files[i].readlines(1000000) # arg is bytes
+    for i in [ i for i in range(len(files)) if files[i][0] != None and files[i][1] <= 0 ]:
+        lines = files[i][0].readlines(1000000) # arg is bytes
         if not lines:
-            files[i].close()
-            files[i] = None
+            files[i][0].close()
+            files[i][0] = None
         else:
             for line in lines:
                 k, v = line.strip().split("\t")
-                heappush( q, (k,v,i) )
-                bufs[i] += 1
-    files = [ f for f in files if f ]
-    bufs = [ b for b in bufs if b > 0 ]
-    if not files:
+                heappush( q, (k,v,files[i]) )
+                files[i][1] += 1
+    if not q:
         break
-    k, v, j = heappop(q)
-    bufs[j] -= 1
+    k, v, f = heappop(q)
+    f[1] -= 1
     print "%s\t%s\n" % (k,v)
