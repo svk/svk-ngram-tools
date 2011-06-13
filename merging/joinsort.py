61703552397e235b8df8b230b0bed98d549ef169
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-06-13 18:52:53 +0200

    must go much faster. use a heap?

--- merging/joinsort.py
+++ merging/joinsort.py
@@ -1,27 +1,24 @@
 from sys import stdin, stderr, argv
 
 files = list( map( lambda fn : open(fn, "r"), argv[1:] ) )
-bufs = [ None for i in range(len(files)) ]
+bufs = [ 0 for i in range(len(files)) ]
+q = []
 
 while files:
-    for i in range(len(files)):
-        if not bufs[i]:
-            lines = files[i].readlines(1000000) # arg is bytes
-            if not lines:
-                files[i].close()
-                files[i] = None
-            else:
-                bufs[i] = list( map( lambda line : line.strip().split("\t"), lines ) )
+    for i in [ i for i in range(len(files)) if bufs[i] <= 0 ]:
+        lines = files[i].readlines(1000000) # arg is bytes
+        if not lines:
+            files[i].close()
+            files[i] = None
+        else:
+            for line in lines:
+                k, v = line.strip().split("\t")
+                heappush( q, (k,v,i) )
+                bufs[i] += 1
     files = [ f for f in files if f ]
-    bufs = [ b for b in bufs if b ]
+    bufs = [ b for b in bufs if b > 0 ]
     if not files:
         break
-#    j = min( range(len(bufs)), key = lambda i : bufs[i][0][0] ) # GAH not present in old Python
-    j = 0
-    best = None
-    for k in range(len(bufs)):
-        if (best == None) or (bufs[j][0][0] < best):
-            j = k
-            best = bufs[k][0][0]
-    key, value = bufs[j].pop(0)
-    print "%s\t%s" % (key,value)
+    k, v, j = heappop(q)
+    bufs[j] -= 1
+    print "%s\t%s\n" % (k,v)
