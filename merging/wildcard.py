91c591f028e020a53ccf3163a86c32ea9996dfe2
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-09-14 16:30:48 +0200

    comment from the past

--- merging/wildcard.py
+++ merging/wildcard.py
@@ -1,5 +1,15 @@
 from sys import stdin
 
+# as of now this generates all variants (2**n, 32).
+
+# almost certainly we can exclude some of them;
+# for instance, wildcards at the beginning or
+# at the end are not useful.
+# that alone (the other possibility would be
+# restricting to one contiguous stretch of wcs)
+# reduces us to 2**(n-2) meaning max 8.
+# this is almost certainly manageable
+
 wildcard = "<*>"
 
 def variants( xs ):
